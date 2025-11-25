7024d84904162ea90c3ad288ae14fc02b724b0fd
Alexander Nollet <alexanderacj13@gmail.com>
2025-11-25 14:16:54 +0100

    packaged as a plugin

--- addons/network_orchestrator/plugin.gd
+++ addons/network_orchestrator/plugin.gd
@@ -2,22 +2,20 @@
 extends EditorPlugin
 
 func _enter_tree() -> void:
-	var icon = get_editor_interface().get_base_control().get_icon("MultiplayerSynchronizer", "EditorIcons")
 	# Initialization of the plugin goes here.
 	add_custom_type(
 		"AuthoritativeNetworkOrchestrator",
 		"NetworkOrchestrator",
 		preload("lib/orchestrator/authoritative_network_orchestrator.gd"),
-		icon
+		preload("MultiplayerSynchronizer.svg")
 	)
 
 	add_custom_type(
 		"ClientNetworkOrchestrator",
 		"NetworkOrchestrator",
 		preload("lib/orchestrator/client_network_orchestrator.gd"),
-		icon
+		preload("MultiplayerSynchronizer.svg")
 	)
-
 
 func _exit_tree() -> void:
 	# Clean-up of the plugin goes here.
